b83f8eac30ce719d971d62f6041e92acbd92545e
Lenin S <lenin@Lenins-MacBook-Air.local>
2018-03-28 09:29:43 +0530

    Initail commit

---
+++ CardViewSample/TableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  TableViewController.swift
+//  CardViewSample
+//
+//  Created by Lenin S on 28/03/18.
+//  Copyright © 2018 Lworld. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! tableCell
+        
+        DispatchQueue.main.async
+            {
+                cell.backView.layer.cornerRadius = 10.0;
+                cell.frontView.roundCorners([.topRight, .bottomRight], radius: 10)
+            }
+        
+        return cell
+    }
+    
+}
+
+class tableCell : UITableViewCell
+{
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var frontView: UIView!
+}
+
+extension UIView {
+    
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
+}
